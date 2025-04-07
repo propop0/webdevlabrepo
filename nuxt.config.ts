e0dc48adde8968867867f2d601231a191604f085
propop0 <bohdan.novachok@oa.edu.ua>
2025-04-07 09:01:24 +0300

    feat: List of goods created

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-
   modules: [
+    '@nuxt/ui', // ← обов’язково є
     '@nuxt/content',
-    '@nuxt/ui',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils'
-  ]
+  ],
+
+  compatibilityDate: '2025-04-07'
 })
---
+++ pages/products.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Список продуктів</h1>
+    <ul>
+      <li>Хліб</li>
+      <li>Яйця</li>
+      <li>Масло</li>
+      <li>Молоко</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: 'Список продуктів'
+})
+</script>
